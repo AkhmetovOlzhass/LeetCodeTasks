ec5310e76c2e85d72a971e4916a9bce09f06388b
Olzhas Akhmetov <58104696+AkhmetovOlzhass@users.noreply.github.com>
2025-04-01 14:42:33 +0500

    Time: 3 ms (11.9%), Space: 8.9 MB (7.94%) - LeetHub

---
+++ 1086-divisor-game/1086-divisor-game.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool divisorGame(int n) {
+        vector<int> dp(n+1, false);
+        dp[1] = false;
+
+        for(int i = 2; i<=n; i++){
+            for(int j = 1; j<i; j++){
+                 if (i % j == 0 && !dp[i - j]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+         return dp[n];
+
+    }
+};
