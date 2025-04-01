ce2d3e6e89f1ff603d01559eff905853c8b1452c
Olzhas Akhmetov <58104696+AkhmetovOlzhass@users.noreply.github.com>
2025-04-01 13:14:50 +0500

    Time: 429 ms (5.04%), Space: 157.6 MB (43.93%) - LeetHub

---
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> queryResults(int limit, vector<vector<int>>& queries) {
+        vector<int> answer;
+
+        unordered_map<int, int> ball_map;
+        unordered_map<int, int> color_freq;
+
+        for(int i = 0; i<queries.size(); i++){
+            if(ball_map.count(queries[i][0])){
+                int old_color = ball_map[queries[i][0]];
+                color_freq[old_color] -= 1;
+
+                if(color_freq[old_color] == 0){
+                    color_freq.erase(old_color);
+                }
+            }
+
+            ball_map[queries[i][0]] = queries[i][1];
+            color_freq[queries[i][1]] += 1;
+
+            answer.push_back(color_freq.size());
+        }
+
+        for(int i = 0; i<answer.size(); i++){
+            cout << answer[i];
+        }
+        return answer;
+    }
+};
