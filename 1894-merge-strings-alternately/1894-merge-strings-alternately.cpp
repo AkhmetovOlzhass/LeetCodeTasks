ae2436b7a81e0c771d3f30db798d0a113d62ec68
Olzhas Akhmetov <58104696+AkhmetovOlzhass@users.noreply.github.com>
2025-05-28 14:22:12 +0500

    Time: 4 ms (32%), Space: 8.2 MB (96.33%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -3,15 +3,21 @@ public:
     string mergeAlternately(string word1, string word2) {
         string res = "";
 
-        int i = 0;
-        int j = 0;
+        // while(i < word1.size() || j<word2.size()){
+        //     if(i<word1.size()){
+        //         res.push_back(word1[i++]);
+        //     }
+        //     if(j<word2.size()){
+        //         res.push_back(word2[j++]);
+        //     }
+        // }
 
-        while(i < word1.size() || j<word2.size()){
-            if(i<word1.size()){
-                res.push_back(word1[i++]);
+        for(int i = 0; i< max(word1.size(), word2.size()); i++){
+            if(i< word1.size()){
+                res.push_back(word1[i]);
             }
-            if(j<word2.size()){
-                res.push_back(word2[j++]);
+            if(i < word2.size()){
+                res.push_back(word2[i]);
             }
         }
 
