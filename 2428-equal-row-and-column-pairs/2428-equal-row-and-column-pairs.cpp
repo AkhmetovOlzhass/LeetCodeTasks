21e22ff185730ef1164af55779ead99f61f07498
Olzhas Akhmetov <58104696+AkhmetovOlzhass@users.noreply.github.com>
2025-06-02 11:36:22 +0500

    Time: 16 ms (64.36%), Space: 37 MB (55.61%) - LeetHub

--- 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.cpp
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
-
-        vector<vector<int>> columnFinal;
-        for(int i =0; i<grid.size(); i++){
-            vector<int> column;
-            for(int j =0; j<grid.size();j++){
-                column.push_back(grid[j][i]);
-            }
-            columnFinal.push_back(column);
-        }
-
-        map<vector<int>, int> res;
+        map<vector<int>, int> rowMap;
 
         for(int i = 0; i<grid.size();i++){
-            res[grid[i]]++;
+            rowMap[grid[i]]++;
         }
 
         int counter = 0;
-        for (int i = 0; i < grid.size(); i++) {
-            counter += res[columnFinal[i]];
 
-            for (int x : columnFinal[i]) cout << x << " ";
-            cout << ": " << res[columnFinal[i]] << endl;
+        for(int i = 0; i<grid.size(); i++){
+            vector<int> col;
+
+            for(int j =0;j < grid.size();j++){
+                col.push_back(grid[j][i]);
+            }
+
+            counter += rowMap[col];
         }
 
         return counter;
