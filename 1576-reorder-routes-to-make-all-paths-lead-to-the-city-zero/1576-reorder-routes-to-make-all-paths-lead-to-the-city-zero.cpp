a3907a0c7e2d0afb8d19b01ec307a43b8290c0b8
Olzhas Akhmetov <58104696+AkhmetovOlzhass@users.noreply.github.com>
2025-06-11 16:15:03 +0500

    Time: 410 ms (10.09%), Space: 147.8 MB (16.93%) - LeetHub

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -2,34 +2,40 @@ class Solution {
 public:
     int minReorder(int n, vector<vector<int>>& connections) {
         vector<vector<int>> graph(n);
-        unordered_set<string> directed;
-        for(auto& con : connections){   
+        unordered_set<string> st;
+        vector<bool> visited(n,false);
+
+        for(auto& con : connections){
             int from = con[0];
             int to = con[1];
             graph[from].push_back(to);
             graph[to].push_back(from);
-            directed.insert(to_string(from) + "->" + to_string(to));
+            st.insert(to_string(from) + "->" + to_string(to));
         }
-        vector<bool> visited(n, false);
+
         int count = 0;
 
-        dfs(0, graph, directed, visited, count);
+        dfs(0, graph, st, visited, count);
 
         return count;
+
+
     }
 
-    void dfs(int node, vector<vector<int>>& graph, unordered_set<string>& directed,  vector<bool>& visited, int& count){
+    void dfs(int node,vector<vector<int>>& graph, unordered_set<string>& st, vector<bool>& visited, int& count){
         visited[node] = true;
 
         for(int neighbor : graph[node]){
-            
             if(!visited[neighbor]){
-                string edge = to_string(node) + "->" + to_string(neighbor);
-                if (directed.count(edge)) {
+                string direct = to_string(node) + "->" + to_string(neighbor);
+                if(st.count(direct)){
                     count++;
                 }
-                dfs(neighbor, graph,directed, visited, count);
+                dfs(neighbor, graph, st, visited, count);
             }
         }
     }
+
+
+
 };
