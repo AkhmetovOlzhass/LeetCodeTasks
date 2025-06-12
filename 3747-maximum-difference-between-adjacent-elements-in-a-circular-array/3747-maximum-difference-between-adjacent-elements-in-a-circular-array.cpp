f6d388ce29dfb1b5827c85486b26aebe83db7ff8
Olzhas Akhmetov <58104696+AkhmetovOlzhass@users.noreply.github.com>
2025-06-12 11:51:38 +0500

    Time: 0 ms (100%), Space: 30.6 MB (23.04%) - LeetHub

--- 3747-maximum-difference-between-adjacent-elements-in-a-circular-array/3747-maximum-difference-between-adjacent-elements-in-a-circular-array.cpp
+++ 3747-maximum-difference-between-adjacent-elements-in-a-circular-array/3747-maximum-difference-between-adjacent-elements-in-a-circular-array.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     int maxAdjacentDistance(vector<int>& nums) {
-        int j = 1;
         int maxSum = INT_MIN;
         int sum = 0;
         for(int i = 0; i<nums.size();i++){
-            if(i == nums.size()-1){
-                j = 0;
-            }
-            
+            int j = (i + 1) % nums.size();
             sum = nums[i] - nums[j];
-            j++;
             maxSum = max(maxSum, abs(sum));
 
         }
