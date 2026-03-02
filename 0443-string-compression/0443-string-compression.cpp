7da455f4cb8fd74b8b28a01bd71124813a1885ff
Olzhas Akhmetov <58104696+AkhmetovOlzhass@users.noreply.github.com>
2026-03-02 12:56:46 +0500

    Time: 0 ms (100%), Space: 13.9 MB (46.24%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,42 +1,30 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        vector<char> finalStr;
-        char c = chars[0];
-        int counter = 1;
+        int i = 0;
+        int res = 0;
+        
 
-        if( chars.size() == 1){
-            return 1;
-        }
-        for(int i = 1; i< chars.size(); i++){
-            if(chars[i] != c){
-                finalStr.push_back(c);
-                if (counter > 1) {
-                    string cnt = to_string(counter);
-                    for (char digit : cnt) {
-                        finalStr.push_back(digit);
-                    }
+        while(i < chars.size()){
+            int groupLength = 1;
+            while(i+groupLength<chars.size() && chars[i] == chars[i+groupLength]){
+                groupLength++;
+            }
+            chars[res] = chars[i];
+            res++;
+
+            if(groupLength>1){
+                for(int j = 0; j<to_string(groupLength).size(); j++){
+                    chars[res] = to_string(groupLength)[j];
+                    res++;
                 }
-                c = chars[i];
-                counter = 1;
-            } else{
-                counter++;
             }
+
+            i+=groupLength;
+
+
         }
 
-
-
-        finalStr.push_back(c);
-        if (counter > 1) {
-            string cnt = to_string(counter);
-            for (char digit : cnt) {
-                finalStr.push_back(digit);
-            }
-        }
-
-
-        chars = finalStr;
-
-        return chars.size();
+        return res;
     }
 };
